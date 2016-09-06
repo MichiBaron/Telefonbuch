df38bad1b0b5b13e3f016edeb6647f6d4e00aa0d
mikebaron80 <mikebaron80@gmail.com>
2016-09-07 00:47:48 +0200

    halbzeit Für die eingabe der Daten ist gesorgt

--- Telefonbuch/MainWindow.xaml.cs
+++ Telefonbuch/MainWindow.xaml.cs
@@ -26,45 +26,45 @@ namespace Telefonbuch
             InitializeComponent();
         }
 
-        private void btnSpeichern_Click(object sender, RoutedEventArgs e)
-        {
-            this.tbEintrag.Text += TextSpeichern(this.btnSpeichern.Name);
-        }
-        string TextSpeichern(string sKnopf)
-        {
+        //private void btnSpeichern_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.tbEintrag.Text += TextSpeichern(this.btnSpeichern.Name);
+        //}
+        //string TextSpeichern(string sKnopf)
+        //{
            
-            return sKnopf + "\n" + "Vorname: " + this.tbVorname.Text + "\nName: " + this.tbName.Text + "\nTelefonnummer: " + this.tbTeleNum.Text + "\n\n";
-        }
+        //    return sKnopf + "\n" + "Vorname: " + this.tbVorname.Text + "\nName: " + this.tbName.Text + "\nTelefonnummer: " + this.tbTeleNum.Text + "\n\n";
+        //}
 
-        private void btnLoeschen_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.cbAlles.IsChecked == true)
-            {
-                MessageBoxResult MR = MessageBox.Show("Wirklich alles Löschen?", "Löschen?", MessageBoxButton.YesNo, MessageBoxImage.Warning,MessageBoxResult.No);
+        //private void btnLoeschen_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (this.cbAlles.IsChecked == true)
+        //    {
+        //        MessageBoxResult MR = MessageBox.Show("Wirklich alles Löschen?", "Löschen?", MessageBoxButton.YesNo, MessageBoxImage.Warning,MessageBoxResult.No);
 
-                if (MR == MessageBoxResult.Yes)
-                {
-                    DeleteAlles();
-                }
-            }
-            else
-            {
-                DeleteOben();
+        //        if (MR == MessageBoxResult.Yes)
+        //        {
+        //            DeleteAlles();
+        //        }
+        //    }
+        //    else
+        //    {
+        //        DeleteOben();
                 
-            }
-        }
-        void DeleteOben()
-        {
-            this.tbName.Text = "";
-            this.tbVorname.Text = "";
-            this.tbTeleNum.Text = "";
-        }
-        void DeleteAlles()
-        {
-            this.tbName.Text = "";
-            this.tbVorname.Text = "";
-            this.tbTeleNum.Text = "";
-            this.tbEintrag.Text = "";
-        }
+        //    }
+        //}
+        //void DeleteOben()
+        //{
+        //    this.tbName.Text = "";
+        //    this.tbVorname.Text = "";
+        //    this.tbTeleNum.Text = "";
+        //}
+        //void DeleteAlles()
+        //{
+        //    this.tbName.Text = "";
+        //    this.tbVorname.Text = "";
+        //    this.tbTeleNum.Text = "";
+        //    this.tbEintrag.Text = "";
+        //}
     }
 }
